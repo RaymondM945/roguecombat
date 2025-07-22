6823b105f107d30c5276bbced9e12523b515079a
RaymondM945 <raymondm9820@gmail.com>
2025-07-21 21:49:53 -0400

    commit

--- combatrogue.lua
+++ combatrogue.lua
@@ -1,4 +1,4 @@
-local isFollowing = true
+local isFollowing = nil
 
 local box = CreateFrame("Frame", "CombatRogueCenterBox", UIParent)
 box:SetSize(25, 25)
@@ -17,7 +17,7 @@ box:SetScript("OnUpdate", function(self, elapsed)
 		local comboPoints = GetComboPoints("player", "target")
 		local sametarget = UnitIsUnit("target", "party1target")
 
-		if UnitAffectingCombat("player") and hpPercent < 90 then
+		if UnitAffectingCombat("party1") and hpPercent < 90 then
 			box.texture:SetColorTexture(1, 1, 0, 1)
 			local SinisterStrike = GetSpellInfo(1752)
 			local SSusable = IsUsableSpell(SinisterStrike)
@@ -31,9 +31,9 @@ box:SetScript("OnUpdate", function(self, elapsed)
 			elseif not IsCurrentSpell("Attack") then
 				box.texture:SetColorTexture(0, 1, 0, 1)
 			elseif comboPoints >= 4 and isEviscerateUsable then
-				box.texture:SetColorTexture(1, 0, 0, 1)
+				box.texture:SetColorTexture(1, 0, 1, 1)
 			elseif SSusable then
-				box.texture:SetColorTexture(0, 1, 0, 1)
+				box.texture:SetColorTexture(1, 0, 0, 1)
 			end
 		end
 	end
@@ -45,10 +45,10 @@ box:RegisterEvent("AUTOFOLLOW_END")
 box:SetScript("OnEvent", function(self, event, ...)
 	if event == "AUTOFOLLOW_BEGIN" then
 		local name = ...
-		isFollowing = true
+		isFollowing = name
 		print("Now following: " .. (name or "unknown"))
 	elseif event == "AUTOFOLLOW_END" then
-		isFollowing = false
+		isFollowing = nil
 		print("Stopped following")
 	end
 end)
